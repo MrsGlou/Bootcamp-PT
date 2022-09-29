a37e145f5a17c2ba1cff1b9ade2b9871deeb090f
Andrea <andrea.pewa@gmail.com>
2022-09-29 19:40:25 +0200

    Iteration#1

---
+++ Javascript/JS Basic Algorithms/app.js
@@ -0,0 +1,11 @@
+/*---------------------------------------------------
+-------------****Iteración #1: Variables**-----------
+---------------------------------------------------*/
+
+//1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+let myFavoriteHero = "Hulk";
+
+//1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+let x = 50;
