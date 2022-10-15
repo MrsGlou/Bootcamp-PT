72521dcfc87406fd71ca2ce9a97881301a57503d
Andrea <andrea.pewa@gmail.com>
2022-10-15 13:45:11 +0200

    Iteration#5

--- Javascript/JS Loops/app5.js
+++ Javascript/JS Loops/app5.js
@@ -13,20 +13,11 @@ const placesToTravel = [
 
 const destiny = [];
 
-for (let i = 0; i < placesToTravel.length; i++) {
-  if (placesToTravel[i].id == 11 || placesToTravel[i].id == 40) {
-  } else {
-    destiny.push(placesToTravel);
-  }
-}
-
-console.log(destiny);
-/*
 for (let placeToTravel of placesToTravel) {
-  if (placeToTravel.id == 11 || placeToTravel.id == 40)  {    
+  if (placeToTravel.id == 11 || placeToTravel.id == 40) {
   } else {
     destiny.push(placeToTravel);
   }
 }
 
-console.log(destiny);*/
+console.log(destiny);
