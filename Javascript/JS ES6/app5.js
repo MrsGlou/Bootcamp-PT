40205a55bbf3063387ceb894d19aab0cfb3b4195
Andrea <andrea.pewa@gmail.com>
2022-10-20 19:17:27 +0200

    Iteration#5

---
+++ Javascript/JS ES6/app5.js
@@ -0,0 +1,30 @@
+/*--------------------------------------
+---------**Iteración #5: Filter**-------
+--------------------------------------*/
+
+//5.1 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los valores que sean mayor que 18
+
+const ages = [22, 14, 24, 55, 65, 21, 12, 13, 90];
+
+const morethan18 = ages.filter(age=> age >18);
+console.log(morethan18);
+
+//5.2 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los valores que sean par.
+
+const par= ages.filter(age=> age%2 == 0);
+console.log(par);
+
+//5.3 Dado el siguiente array, utiliza .filter() para generar un nuevo array 
+//con los streamers que tengan el gameMorePlayed = 'League of Legends'.
+
+const streamers = [
+	{name: 'Rubius', age: 32, gameMorePlayed: 'Minecraft'},
+	{name: 'Ibai', age: 25, gameMorePlayed: 'League of Legends'}, 
+	{name: 'Reven', age: 43, gameMorePlayed: 'League of Legends'},
+	{name: 'AuronPlay', age: 33, gameMorePlayed: 'Among Us'}
+];
+
+const lol = streamers.filter (streamer => streamer.gameMorePlayed == "League of Legends");
+console.log(lol);
