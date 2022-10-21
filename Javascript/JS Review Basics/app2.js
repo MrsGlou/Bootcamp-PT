a01ede5468af2f8eb26f01b68573d0d222321ff9
Andrea <andrea.pewa@gmail.com>
2022-10-21 20:31:58 +0200

    Iteration#2

--- Javascript/JS Review Basics/app2.js
+++ Javascript/JS Review Basics/app2.js
@@ -2,57 +2,6 @@
 -------**Iteración #2: Mix Fors**-------
 ---------------------------------------*/
 
-const users1 = [
-  {
-    name: "Manolo el del bombo",
-    favoritesSounds: {
-      waves: { format: "mp3", volume: 50 },
-      rain: { format: "ogg", volume: 60 },
-      firecamp: { format: "mp3", volume: 80 },
-    },
-  },
-  {
-    name: "Mortadelo",
-    favoritesSounds: {
-      waves: { format: "mp3", volume: 30 },
-      shower: { format: "ogg", volume: 55 },
-      train: { format: "mp3", volume: 60 },
-    },
-  },
-  {
-    name: "Super Lopez",
-    favoritesSounds: {
-      shower: { format: "mp3", volume: 50 },
-      train: { format: "ogg", volume: 60 },
-      firecamp: { format: "mp3", volume: 80 },
-    },
-  },
-  {
-    name: "El culebra",
-    favoritesSounds: {
-      waves: { format: "mp3", volume: 67 },
-      wind: { format: "ogg", volume: 35 },
-      firecamp: { format: "mp3", volume: 60 },
-    },
-  },
-];
-
-
-/*
-for (let user of users) {
-  for (favoriteSounds in user.favoritesSounds) {
-    if (!mediaVol.includes(favoriteSounds)) {
-      const repeat = favoriteSounds.reduce((favoriteSounds[i-1], favoriteSounds[i-1]) => favoriteSounds[i-1] + favoriteSounds[i-1], 0 );
-      mediaVol.push(favoriteSounds, repeat);
-    }
-  }
-}
-
-console.log(mediaVol);
-
-Hay que hacerlo dentro del 2 for, vamos haciendo la media por veces que sale un sonido 
-const promedio = mediaVol / favoriteSounds[i].length;
-*/
 const users = [
   {
     name: "Manolo el del bombo",
@@ -88,30 +37,30 @@ const users = [
   },
 ];
 
-const arrayFavoritesSounds = [];
-const sounds = [];
-const volume = [];
-let counter = 0;
-let denominador = 0;
+//Necesitamos una variable vacia para guardar la media
+let mediaVol;
 
-for (const user of users) {
-  for (const key in user) {
-    //console.log(user.favoritesSounds);
-    arrayFavoritesSounds.unshift(user.favoritesSounds);
+//Necesitamos guardar los volumenes en un array
+let sounds = [];
+
+//Recorremos el array
+for (let user of users) {
+  //Recorremos el objeto
+  for (key in user.favoritesSounds) {
+    //Metemos solamente los valores del volumen .push
+    sounds.push(user.favoritesSounds[key].volume);
   }
 }
 
-for (const sound of arrayFavoritesSounds) {
-  for (const typeOfSound in sound) {
-    sounds.unshift(Object.values(sound));
+const mediaFunct = (array) => {
+  let acumulador = 0;
+  for (let volume of array) {
+    acumulador += volume;
   }
-}
+  //media = suma total / cantidad de elementos
+  let media = acumulador / array.length;
+  return media;
+};
 
-for (const type of sounds) {
-  for (const feature of type) {
-    counter += feature.volume;
-    denominador++;
-  }
-}
-const media = counter/denominador;
-console.log(media);
+mediaVol = mediaFunct(sounds);
+console.log(mediaVol);
