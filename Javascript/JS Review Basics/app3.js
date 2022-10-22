26fee0b2d3e6236d4ee51493d1c9bed799618b8d
Andrea <andrea.pewa@gmail.com>
2022-10-22 17:40:30 +0200

    Iteration#3

--- Javascript/JS Review Basics/app3.js
+++ Javascript/JS Review Basics/app3.js
@@ -1,49 +1,61 @@
 /*--------------------------------------
--------**Iteración #2: Mix Fors**-------
+-------**Iteración #3: Mix Fors**-------
 ---------------------------------------*/
 
 const users = [
-    {
-      name: "Manolo el del bombo",
-      favoritesSounds: {
-        waves: { format: "mp3", volume: 50 },
-        rain: { format: "ogg", volume: 60 },
-        firecamp: { format: "mp3", volume: 80 },
-      },
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
     },
-    {
-      name: "Mortadelo",
-      favoritesSounds: {
-        waves: { format: "mp3", volume: 30 },
-        shower: { format: "ogg", volume: 55 },
-        train: { format: "mp3", volume: 60 },
-      },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
     },
-    {
-      name: "Super Lopez",
-      favoritesSounds: {
-        shower: { format: "mp3", volume: 50 },
-        train: { format: "ogg", volume: 60 },
-        firecamp: { format: "mp3", volume: 80 },
-      },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
     },
-    {
-      name: "El culebra",
-      favoritesSounds: {
-        waves: { format: "mp3", volume: 67 },
-        wind: { format: "ogg", volume: 35 },
-        firecamp: { format: "mp3", volume: 60 },
-      },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
     },
-  ];
-  
-  const mediaVol = [];
-  
-  for (let user of users) {
-    for (favoriteSounds in user.favoritesSounds) {
-      mediaVol.push(favoriteSounds);
+  },
+];
+
+const mediaVol = [];
+//Recorremos todo el array y creamos un array solamente con todos los sonidos
+for (let user of users) {
+  for (favoriteSounds in user.favoritesSounds) {
+    mediaVol.push(favoriteSounds);
+  }
+}
+//Vemos cuantas veces se repite un sonido dentro del nuevo array
+function repeatCounter(sound) {
+  let indice = [];
+  for (let i = 0; i < sound.length; i++) {
+    if (indice[sound[i]]) {
+      //Comprobamos que la key esta dentro del nuevo objeto.
+      indice[sound[i]] = indice[sound[i]] + 1;
+    } else {
+      indice[sound[i]] = 1;
     }
   }
-  
-  console.log(mediaVol);
-  
+  return indice;
+}
+
+console.log(repeatCounter(mediaVol));
