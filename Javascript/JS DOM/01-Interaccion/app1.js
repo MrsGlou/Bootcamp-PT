df97afd8177b1ab9ea0f229de37a761473ec3fd8
Andrea <andrea.pewa@gmail.com>
2022-10-26 20:16:56 +0200

    Iteration#1

--- Javascript/JS DOM/01-Interaccion/app1.js
+++ Javascript/JS DOM/01-Interaccion/app1.js
@@ -14,16 +14,22 @@ console.log(recoverPillado);
 //1.3 Usa querySelector para mostrar por consola todos los p
 //Para mostrar "p" no hace falta . ni #
 const recoverP = document.querySelectorAll("p");
-console.log(recoverP);
+recoverP.forEach(p => {
+    console.log(p);
+});
 
 //1.4 Usa querySelector para mostrar por consola todos los elementos con la clase.pokemon
 const recoverPokemon = document.querySelectorAll(".pokemon");
-console.log(recoverPokemon);
+recoverPokemon.forEach(pokemon => {
+    console.log(pokemon);
+});
 
 //1.5 Usa querySelector para mostrar por consola todos los elementos con el atributo data-function="testMe".
 const recoverData = document.querySelectorAll("[data-function = 'testMe']");
-console.log(recoverData);
+recoverData.forEach(data => {
+    console.log(data);
+});
 
 //1.6 Usa querySelector para mostrar por consola el 3 personaje con el atributo data-function="testMe".
-const recoverData3 = document.querySelectorAll("data-function = child");
-console.log(recoverData3);
+const recoverData3 = document.querySelectorAll("[data-function = 'testMe']");
+console.log(recoverData3[2]);
