3f2d6e264a008d5d78615f6475b72d49377bce88
Andrea Perez <andreaperez@MacBook-Air-de-Andrea.local>
2022-10-29 15:43:47 +0200

    Iteration#1

--- Javascript/JS Review DOM/app.js
+++ Javascript/JS Review DOM/app.js
@@ -13,15 +13,43 @@ for (const country of countries) {
 document.body.appendChild(list);
 
 //****1.2 Elimina el elemento que tenga la clase .fn-remove-me.
-const removeElement = document.querySelectorAll(".fn-remove-me");
-document.body.removeChild(removeElement);
+const deleteElement = document.querySelector(".fn-remove-me");
+document.body.removeChild(deleteElement);
 
-//1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos 
+//1.3 Utiliza el array para crear dinamicamente una lista ul > li de elementos
 //en el div de html con el atributo data-function="printHere".
-const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+const cars = ["Mazda 6", "Ford fiesta", "Audi A4", "Toyota corola"];
+
+const div1 = document.querySelector('[data-function="printHere"]');
 const list2 = document.createElement("ul");
-//const div1 = document.querySelectorAll("[data-function='printHere']");
+div1.appendChild(list2);
+
 for (const car of cars) {
   list2.innerHTML += `<li>${car}</li>`;
 }
-document.body.appendChild(list2);
+
+//1.4 Crea dinamicamente en el html una lista de div que contenga un elemento
+//h4 para el titulo y otro elemento img para la imagen.
+
+const countries1 = [
+  { title: "China", imgUrl: "https://picsum.photos/300/200?random=1" },
+  { title: "España", imgUrl: "https://picsum.photos/300/200?random=2" },
+  { title: "Portigal", imgUrl: "https://picsum.photos/300/200?random=3" },
+  { title: "Brasil", imgUrl: "https://picsum.photos/300/200?random=4" },
+  { title: "Italia", imgUrl: "https://picsum.photos/300/200?random=5" },
+];
+
+const div2 = document.createElement("div");
+document.body.appendChild(div2);
+countries1.forEach(element => {
+  div2.innerHTML += `<h4>${element.title}</h4><img>${element.imgUrl}</img>`
+});
+
+//1.5 Basandote en el ejercicio anterior. Crea un botón que elimine el último 
+//elemento de la lista.
+const btn = document.createElement("button");
+let newText = document.createTextNode("Pulsame");
+btn.appendChild(newText)
+document.body.appendChild(btn);
+
+btn.addEventListener("click", countries1.pop());
