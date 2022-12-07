537e5a1a27fb5af6305df7ec798f10d229edd187
Andrea <andrea.pewa@gmail.com>
2022-12-07 13:40:26 +0100

    Interation#1

--- Javascript/JS Events/app1.js
+++ Javascript/JS Events/app1.js
@@ -4,12 +4,12 @@
 
 //1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
 //evento click que ejecute un console log con la información del evento del click
-const btn = document.getElementById("btnToClick").addEventListener("click", console.log("click"));
+const btn = document.getElementById("btnToClick").addEventListener("click", (btn) => {console.log(btn)});
 
 //1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
 const focus = document.querySelector(".focus");
-focus.addEventListener("focus", console.log("focus"))
+focus.addEventListener("focus", (evt) =>{console.log(evt.target.value)})
 
 //1.3 Añade un evento 'input' que ejecute un console.log con el valor del input.
 const inputValue = document.querySelector(".value")
-inputValue.addEventListener("value", console.log(inputValue.value));
+inputValue.addEventListener("input", (evt) => {console.log(evt.target.value)});
