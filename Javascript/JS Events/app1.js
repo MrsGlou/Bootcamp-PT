df0ec39612ab65b2af0a962bbe66673128fe2592
Andrea <andrea.pewa@gmail.com>
2022-12-07 14:30:29 +0100

    Iteración #1: Fetch

--- Javascript/JS Events/app1.js
+++ Javascript/JS Events/app1.js
@@ -2,8 +2,7 @@
 ---------**Iteración #1: Creando Events**----------
 -------------------------------------------------*/
 
-//1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el 
-//evento click que ejecute un console log con la información del evento del click
+//1.1 Añade un botón a tu html con el id btnToClick y en tu javascript añade el evento click que ejecute un console log con la información del evento del click
 const btn = document.getElementById("btnToClick").addEventListener("click", (btn) => {console.log(btn)});
 
 //1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input.
