b991cf257a2e3b043a25966e1753bac889233dce
Andrea <andrea.pewa@gmail.com>
2023-01-18 23:44:03 +0100

    proyect-components-advanced

---
+++ react/Project-components-advanced/src/components/Title/Title.jsx
@@ -0,0 +1,6 @@
+
+const Title = ({text}) => {
+  return <h1 className="title">{text}</h1>;
+};
+
+export default Title;
---
+++ react/Project-components-advanced/src/components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+export {Header, Footer, Main}
+
+Header()
+Footer()
+Main()
