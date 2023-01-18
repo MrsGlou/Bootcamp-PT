40f4fa60000fa03553ad42e90cd2a3f90f8643b6
MrsGlou <114435154+MrsGlou@users.noreply.github.com>
2023-01-18 13:47:34 +0100

    deleted:    react/Mini Proyecto React 1/.gitignore     deleted:    react/Mini Proyecto React 1/index.html     deleted:    react/Mini Proyecto React 1/package-lock.json 	deleted:    react/Mini Proyecto React 1/package.json 	deleted:    react/Mini Proyecto React 1/public/vite.svg 	deleted:    react/Mini Proyecto React 1/src/App.css 	deleted:    react/Mini Proyecto React 1/src/App.jsx 	deleted:    react/Mini Proyecto React 1/src/assets/react.svg 	deleted:    react/Mini Proyecto React 1/src/index.css 	deleted:    react/Mini Proyecto React 1/src/main.jsx 	deleted:    react/Mini Proyecto React 1/vite.config.js

--- react/mini-proyecto-react-1/src/App.jsx
+++ react/mini-proyecto-react-1/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [characterList, setCharacterList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      let data = await fetch(`https://rickandmortyapi.com/api/character/`).then(
+        (res) => res.json()
+      );
+      setCharacterList(data.results);
+    })();
+  }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <div className="App">
+    
+    {characterList.map((character) => (
+      <div className= "Card" key= {character.id}>
+        <h3>#{character.id}</h3>
+        <h2>{character.name.toUpperCase()}</h2>
+        <h3>{character.origin.name}</h3>
+        <img src={character.image} alt= {character.name} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    ))}
+  </div>
+  );
+};
 
 export default App
