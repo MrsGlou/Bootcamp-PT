98027b02cfb675bd638c2163c038d608a9c0d8b9
Andrea <andrea.pewa@gmail.com>
2023-02-14 00:09:23 +0100

    digital-clock

---
+++ react/Digital-Clock/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countdown from './components/Countdown'
+import DigitalClock from './components/DigitalClock/DigitalClock'
+
+function App() {
+
+  return (
+    <div className="App">
+    <DigitalClock />
+    <Countdown />
+    </div>
+  )
+}
+
+export default App
