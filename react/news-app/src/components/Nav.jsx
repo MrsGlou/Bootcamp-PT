381c025d8e5869c9e099d211fc9a26a0e48bafb0
Andrea <andrea.pewa@gmail.com>
2023-03-14 19:17:05 +0100

    news app

---
+++ react/news-app/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+function Nav () {
+    return (
+        <section>
+            <h1>hola</h1>
+        </section>
+    )
+}
+
+export default Nav
---
+++ react/news-app/src/context/userContext.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const userContext = createContext();
+
+export const ContexProvider = ({ children }) => {
+    const [user, setUser] = useState (() => {
+        const ID = localStorage.getItem("user");
+        return ID ? ID : null;
+    });
+
+    const navigate = useNavigate();
+
+    const login = (user) => {
+        localStorage.setItem("user", user);
+        setUser(user);
+        navigate("/home");
+    };
+
+    const logout = () =>{
+        localStorage.removeItem("user");
+
+        setUser(null)
+    }
+    return (
+        <userContext.Provider value={{ user, login, logout, setUser}}>
+            {children}
+        </userContext.Provider>
+    )
+}
+
