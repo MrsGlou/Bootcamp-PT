0960d963f0a7613ae99a28423c68afc55bb50730
Andrea <andrea.pewa@gmail.com>
2023-04-04 18:07:10 +0200

    animal-crossing-proyect

--- react/animal-crossing-proyect/src/components/Protected.jsx
+++ react/animal-crossing-proyect/src/components/Protected.jsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 
 const Protected = ({ children }) => {
   if (!localStorage.getItem('user')) {
-    return <Navigate to="/" />;
+    return <Navigate to="/login" />;
   }
   return children;
 };
