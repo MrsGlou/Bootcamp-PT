46993bd5111639ffb8512fc64092c92eef5b6b52
Andrea <andrea.pewa@gmail.com>
2023-04-10 13:07:56 +0200

    animal-crossing

---
+++ react/animal-crossing-proyect/src/components/Filters.jsx
@@ -0,0 +1,10 @@
+import { useId, useState } from 'react';
+
+import React from 'react';
+
+const Filters = () => {
+  const { filters, setFilters } = useFilters();
+  return <div>Filters</div>;
+};
+
+export default Filters;
