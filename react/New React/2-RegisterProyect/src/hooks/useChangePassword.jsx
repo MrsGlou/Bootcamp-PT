7318bdbb593715c30a612b5b66e880cd8b63e34e
Andrea <114435154+MrsGlou@users.noreply.github.com>
2023-07-12 11:18:27 +0200

    Delete useChangePassword.jsx

--- react/New React/2-RegisterProyect/src/hooks/useChangePassword.jsx
+++
@@ -1,7 +0,0 @@
-import Swal from "sweetalert2/dist/sweetalert2.all.js";
-
-const useChangePassword = (res, setRes, setForgotPassword) => {
-  return <div>useChangePassword</div>;
-};
-
-export default useChangePassword;
